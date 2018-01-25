bc3cfc14b9f2f542565fec2dc8f8c310473ae92f
Dobby233Liu <papaqi1234567@126.com>
2018-01-25 21:06:06 +0800

    Code edits

--- app/src/main/java/com/github/dobby233liu/droidtime/MainActivity.java
+++ app/src/main/java/com/github/dobby233liu/droidtime/MainActivity.java
@@ -4,8 +4,7 @@ import android.app.*;
 import android.os.*;
 import java.util.Date;
 import android.view.*;
-import android.widget.*;
-import    java.text.SimpleDateFormat;     
+import android.widget.*;    
 
 public class MainActivity extends Activity 
 {
@@ -13,15 +12,22 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) 
 
     {
+    super.onCreate(savedInstanceState);
 	
-        super.onCreate(savedInstanceState);
 	String dateText=new Date().toLocaleString();
+	
 	requestWindowFeature(Window.FEATURE_NO_TITLE);
+	
 	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	
 	setContentView(R.layout.main);
+	
 	TextView viewMe = (TextView)findViewById(R.id.date);   
+	
 	viewMe.setText(dateText);
+	
 	Button btnExit=(Button)findViewById(R.id.ExitButton);
+	
 	btnExit.setOnClickListener(new View.OnClickListener(){
 	    @Override
 		public void onClick(View p1){
